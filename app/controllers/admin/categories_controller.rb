84e629963fc90d048269ad95716f5363f3fc615e
Sanjoy Kumar Das <sanjoy.kumar.backup@gmail.com>
2022-02-07 22:59:43 -0500

    Add Admin Categories.

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,44 @@
+class Admin::CategoriesController < ApplicationController
+  #need to create an index, new and create actions
+
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+
+  def index
+    @category_name_to_id = {}
+    @product_per_category_count = {}
+    @categories = Category.all
+    @product_category = Category.joins(:products)
+
+    @categories.each do |c|
+      @product_per_category_count[c.name] = 0
+      @category_name_to_id[c.name] = c.id
+    end
+
+    @product_category.map do |n|
+      @product_per_category_count[n.name] += 1
+    end
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
+end 
